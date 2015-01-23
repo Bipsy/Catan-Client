48a1ab8a6c9cd328aba6ea8e158707138a43a04a
x3ro <atsoderquist@gmail.com>
2015-01-23 00:01:00 -0700

    added comment

--- src/shared/models/DTO/ClientModelDTO.java
+++ src/shared/models/DTO/ClientModelDTO.java
@@ -1,5 +1,12 @@
 package shared.models.DTO;
 
+/**
+ * This class stores the information needed to create a JSON string of a
+ * game's state, and is used to facilitate the transfer of data between
+ * the server and client.
+ * @author Austin
+ *
+ */
 public class ClientModelDTO {
 	private ResourceListDTO bank;
 	private MessageListDTO chat;
