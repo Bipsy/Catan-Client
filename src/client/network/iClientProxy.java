95f23d0959182547fad0d36fb385ed2f3b11288e
Jillian <jpkoontz@gmail.com>
2015-01-23 11:50:47 -0700

    essentials of interface proxys

--- src/client/network/iClientProxy.java
+++ src/client/network/iClientProxy.java
@@ -5,12 +5,33 @@
  */
 package client.network;
 
+import java.io.IOException;
+import shared.locations.*;
+import shared.definitions.*;
+
 /**
  *
  * @author Jillian Koontz <jpkoontz@gmail.com>
  */
 public interface iClientProxy {
     
+	void sendChat(String content) throws IOException;
+	void acceptTrade(boolean willAccept) throws IOException;
+	void discardCards(ResourceList discardedCards) throws IOException; //ResourceList
+	int rollNumber();
+	void buildRoad(boolean free, EdgeLocation roadLocation) throws IOException;
+	void buildSettlement(boolean free, VertexLocation vertexLocation) throws IOException;
+	void buildCity(VertexLocation vertexLocation) throws IOException;
+	void offerTrade(ResourceList offer, int reciever) throws IOException;
+	void maritimeTrade(int ratio, ResourceType inputResource, ResourceType outputResource) throws IOException;
+	void robPlayer(HexLocation location, int victimIndex) throws IOException; //
+	void finishTurn() throws IOException;
+	void buyDevCard() throws IOException;
+	void playSoldier(HexLocation location, int victimIndex) throws IOException;
+	void playYearOfPlenty(ResourceType resource1, ResourceType resource2) throws IOException;
+	void playRoadBuilding(EdgeLocation spot1, EdgeLocation spot2) throws IOException;
+	void playMonopoly(ResourceType resource) throws IOException;
+	void playMonument() throws IOException;
 	
 	
 }
