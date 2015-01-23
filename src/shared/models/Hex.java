cf986817ae808b271907fcfe4ac1ed264c43c035
Mikey Murphy <mikeyamadeo@gmail.com>
2015-01-23 14:54:53 -0700

    comments

--- src/shared/models/Hex.java
+++ src/shared/models/Hex.java
@@ -8,6 +8,10 @@ import shared.locations.VertexLocation;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a hex on the board.
+ * @author Mikey Murphy <mikeyamadeo@gmail.com>
+ */
 public class Hex {
 	private HexLocation location;
 	private ResourceType resource;
@@ -16,26 +20,50 @@ public class Hex {
 	private Harbor harbor;
 	private Map<VertexLocation, VertexObject> communityMap;
 	
+	/**
+	 * This function returns all the adjacent edges.
+	 * @return list of edges
+	 */
 	public List<EdgeLocation> getAdjacentEdges() {
 		return null;
 	}
 	
+	/**
+	 * This function returns all the adjacent vertices
+	 * @return list of vertices
+	 */
 	public List<VertexLocation> getAdjacentVertices() {
 		return null;
 	}
 	
+	/**
+	 * Returns the hex neighbors
+	 * @return list of hexes
+	 */
 	public List<Hex> getNeighbors() {
 		return null;
 	}
 	
+	/**
+	 * Determines whether or not a road can be build on it.
+	 * @return true if it can, false if not.
+	 */
 	public boolean canBuildRoad() {
 		return false;
 	}
 	
+	/**
+	 * Determines if a settlement can be built
+	 * @return true if so, false if not
+	 */
 	public boolean canBuildSettlement() {
 		return false;
 	}
 	
+	/**
+	 * Determines if a city can be built
+	 * @return true if so, false if not
+	 */
 	public boolean canBuildCity() {
 		return false;
 	}
