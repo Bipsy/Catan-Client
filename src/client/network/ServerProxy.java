075b191f3aca11dae2acf7100d151f85d4542aa8
Jillian <jpkoontz@gmail.com>
2015-01-29 22:07:04 -0700

    beginnings of server connection doPost and doGet

--- src/client/network/ServerProxy.java
+++ src/client/network/ServerProxy.java
@@ -6,6 +6,8 @@
 package client.network;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import shared.definitions.CatanColor;
 import shared.definitions.ResourceType;
@@ -21,10 +23,152 @@ import shared.models.TradeOffer;
  * ServerProxy is our implementation of the iServerProxy interface.
  * This class will be used to handle all communication with the server.
  * @author Peter Anderson <anderson.peter@byu.edu> 
+ * @author Jillian Koontz <jpkoontz@gmail.com>
  */
 public class ServerProxy implements iServerProxy {
 
+	private String serverHost = "localhost";
+	private String serverPort = "8081";
+	
+	/**
+	 * Class constructor
+	 */
+	public ServerProxy() {
+		
+	}
+	
+	/**
+	 * Class constructor
+	 */
+	public ServerProxy(String serverHost, String serverPort) {
+		this.serverHost = serverHost;
+		this.serverPort = serverPort;
+	}
+	
+	private Object doGet(String urlPath) throws IOException {
+		try {
+			URL url = new URL("http://" + serverHost + ":" + serverPort + urlPath);
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accept","JSON");
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			
+			connection.connect();
+			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				Object result = connection.getInputStream();
+				return result;
+			}
+			else {
+				throw new IOException(String.format("doGet failed: %s (http code %d)",
+											urlPath, connection.getResponseCode()));
+			}
+		}
+		catch (IOException e) {
+			throw new IOException(String.format("doGet failed: %s", e.getMessage()), e);
+		}
+	}
+	
+	public Object doPost(String urlPath, Object params) throws IOException {
+		try {
+			URL url = new URL("http://" + serverHost + ":" + serverPort + urlPath);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Accept","JSON");
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			
+			connection.connect();
+			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				//Set cookies
+				//send the result to the serializer
+				Object result = connection.getInputStream();
+				return result;
+			} else {
+				throw new IOException(String.format("doPost failed: %s (http code %d)",
+						urlPath, connection.getResponseCode()));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IOException();
+		}
+	}
+	
 
+    @Override
+    public String login(String username, String password) throws IOException {
+    	//send params to serializer
+    	//user DTO returned
+        //return User doPost("/user/login", user);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String registerNewUser(String username, String password) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String listGames() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String createGames(String name, int randomTiles, int randomNumbers, int randomPorts) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String joinGame(int gameId, CatanColor color) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String saveGames(int gameId, String fileName) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String loadGame() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String retrieveCurrentState(int versionNumber) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String resetGame() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getCommands() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String postGameCommands(CommandContainer commands) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String listAITypes() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String addAIPlayer(AIPlayer player) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String changeLogLevel(int logLevel) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+	
+	
     @Override
     public String sendChat(String content) throws IOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -107,76 +251,6 @@ public class ServerProxy implements iServerProxy {
 
     @Override
     public String playMonument() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String login(String username, String password) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String registerNewUser(String username, String password) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String listGames() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String createGames(String name, int randomTiles, int randomNumbers, int randomPorts) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String joinGame(int gameId, CatanColor color) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String saveGames(int gameId, String fileName) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String loadGame() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String retrieveCurrentState(int versionNumber) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String resetGame() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getCommands() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String postGameCommands(CommandContainer commands) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String listAITypes() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String addAIPlayer(AIPlayer player) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String changeLogLevel(int logLevel) throws IOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
