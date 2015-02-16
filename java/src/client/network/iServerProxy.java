c1c9762ce70ddd96cc88d66ad68d4f67bbe17582
Peter Anderson <anderson.peter@byu.edu>
2015-02-15 19:51:50 -0700

    Symbol resolution

--- java/src/client/network/iServerProxy.java
+++ java/src/client/network/iServerProxy.java
@@ -7,6 +7,7 @@ package client.network;
 
 import java.io.IOException;
 import java.util.List;
+import org.javatuples.Pair;
 
 import shared.models.DTO.ClientModelDTO;
 import shared.models.DTO.GameContainerDTO;
