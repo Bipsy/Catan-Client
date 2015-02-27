eb2035015c3c54fa2a8082b5b277c861e64527e4
x3ro <atsoderquist@gmail.com>
2015-02-27 16:29:48 -0700

    completed rolling

--- java/src/client/roll/RollController.java
+++ java/src/client/roll/RollController.java
@@ -3,14 +3,18 @@ package client.roll;
 import client.base.*;
 import client.model.ModelFacade;
 import client.model.Populator;
+import client.network.ServerProxy;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Random;
 
 import javax.swing.Timer;
+
+import shared.models.DTO.params.RollNumber;
 
 /**
  * Implementation for the roll controller
@@ -18,18 +22,42 @@ import javax.swing.Timer;
 public class RollController extends Controller implements IRollController, Observer {
 	
 	private ModelFacade facade;
+    private static ServerProxy proxy = ServerProxy.getInstance();
 
     private IRollResultView resultView;
     private final String MESSAGE = "Rolling in %d seconds";
-    private final int rollingCount = 3;
+    private int countDown = 3;
+    
     private ActionListener timerListener = new ActionListener() {
+    	   	
         @Override
         public void actionPerformed(ActionEvent e) {
-            Random random = new Random();
-            int rollValue = random.nextInt(12) + 1;
-            getResultView().setRollValue(rollValue);
+        	if(getRollView().isModalShowing()) {
+        		getRollView().setMessage(String.format(MESSAGE, --countDown));
+        		getRollView().showModal();
+        		if(countDown == 0) {
+	        		rollDice();
+	        		((Timer)e.getSource()).stop();
+	        		countDown = 3;
+        		}
+        	}
+        	else {
+        		((Timer)e.getSource()).stop();
+        	}
         }    
     };
+    
+    private int rollDice(int sides, int times) {
+    	
+		Random random = new Random();
+    	
+		int total = 0;
+    	for (int i = 0; i < times; i++) {
+			total += random.nextInt(sides) + 1;
+		}
+    	
+    	return total;
+    }
 
     /**
      * RollController constructor
@@ -57,26 +85,38 @@ public class RollController extends Controller implements IRollController, Obser
     public IRollView getRollView() {
         return (IRollView) getView();
     }
+    
+    protected void showRollModal() {
+
+		getRollView().setMessage(String.format(MESSAGE, countDown));
+		getRollView().showModal();
+		Timer rollingTimer = new Timer(1000, timerListener);
+		rollingTimer.setInitialDelay(1000);
+		rollingTimer.setRepeats(true);
+		rollingTimer.start();
+    }
 
     @Override
     public void rollDice() {
-    	if(facade.isLocalPlayerTurn()) {
-    		String formattedString = String.format(MESSAGE, rollingCount);
-    		getRollView().setMessage(formattedString);
-    		Timer rollingTimer = new Timer(rollingCount, timerListener);
-    		rollingTimer.setInitialDelay(0);
-    		rollingTimer.setRepeats(false);
-    		rollingTimer.start();     
-    		getResultView().showModal();    		
-    	}
+		getRollView().closeModal();
+    	int rollValue = rollDice(6,2);
+		getResultView().setRollValue(rollValue);
+		getResultView().showModal();
+		RollNumber request = new RollNumber(facade.getLocalPlayerIndex(), rollValue);
+		try {
+			proxy.rollNumber(request);
+		} catch (IOException e) {
+			System.err.println(e.toString());
+		}
     }
 
     @Override
     public void update(Observable o, Object arg) {
         if (o instanceof Populator && arg instanceof ModelFacade) {
         	facade = (ModelFacade)arg;
-        	rollDice();
-            
+        	if(facade.isLocalPlayerTurn() && facade.getState().toLowerCase().equals("rolling")) {
+        		showRollModal();
+        	}            
         }
     }
 }
